160d9c4e7473a9c4512d242a164b21c0cc2615a8
Valentina-Gol <vip.valya.golubeva@mail.ru>
2022-09-19 11:41:10 +0300

    added dynamic adding handlers

--- packages/map_editor/classes/layers.py
+++ packages/map_editor/classes/layers.py
@@ -1,44 +1,99 @@
 import logging
-from abc import ABC, abstractmethod
+from abc import ABC
 from dt_maps import MapLayer
 from dt_maps.types.commons import EntityHelper
-from utils.maps import REGISTER
-from mapStorage import MapStorage
+from dt_maps.Map import REGISTER
+from classes.basic.command import Command
 from utils.maps import create_layer
-from typing import Dict, Any
+from mapStorage import MapStorage
+from typing import Dict, Any, Type
+from classes.basic.chain import AbstractHandler
 
 
 class AbstractLayer(ABC):
     _data: MapLayer = None
     _layer_handler: EntityHelper = None
+    _layer_name: str = ""
+    _default_conf: Dict[str, Any] = {}
 
-    def __init__(self) -> None:
+    def __init__(self, layer_name: str) -> None:
         self.dm = MapStorage().map
-        try:
-            self.data = self.dm.layers[self.layer_name()]
-        except KeyError:
-            logging.error(f"Empty layer {self.layer_name()}")
-            create_layer(self.dm, self.layer_name(), {})
-            self.data = self.dm.layers[self.layer_name()]
-        self._layer_handler = REGISTER[self.layer_name()]
+        self._layer_name = layer_name
+        print(layer_name)
 
-    @abstractmethod
+        try:
+            self.data = self.dm.layers[self.layer_name]
+            if layer_name == "vehicles":
+                print(self.data)
+        except KeyError:
+            logging.error(f"Empty layer {self.layer_name}")
+            create_layer(self.dm, self.layer_name, {})
+            self.data = self.dm.layers[self.layer_name]
+        self._layer_handler = REGISTER[self.layer_name]
+
+    @property
     def layer_name(self) -> str:
-        pass
+        return self._layer_name
 
     def render(self) -> None:
         pass
 
-    @abstractmethod
     def default_conf(self) -> Dict[str, Any]:
-        pass
+        return self._default_conf
 
     def check_config(self, config: Dict[str, Any]) -> bool:
+        if not config:
+            return True
         for field in config:
-            map_layer_type = self._layer_handler._get_property_types(self._layer_handler, field)
+            try:
+                map_layer_type = self._layer_handler._get_property_types(
+                    self._layer_handler, field)
+            except TypeError:
+                map_layer_type = self._layer_handler._get_property_types(
+                    field)
             if not isinstance(config[field], map_layer_type):
                 return False
         return True
 
     def set_layer_handler(self, handler: EntityHelper) -> None:
         self._layer_handler = handler
+
+
+class BasicLayerHandler(AbstractLayer, AbstractHandler):
+    def __init__(self, layer_name: str, fields: list = None):
+        super(BasicLayerHandler, self).__init__(layer_name)
+        if fields and len(fields) != 0:
+            conf = {}
+            for field in fields:
+                conf[field] = ""
+            self._default_conf = conf
+
+    def handle(self, command: Command) -> Any:
+        response = command.execute(self.dm, self.data, self.layer_name,
+                                   self.default_conf(),
+                                   check_config=self.check_config)
+        if response:
+            return response
+        return super().handle(command)
+
+
+class DynamicLayer(EntityHelper):
+    _fields: Dict[str, str] = {}
+    _layer_name: str = ""
+    _map = None
+
+    def __init__(self, fields: list, layer_name: str, map):
+        super(DynamicLayer, self).__init__(map, layer_name)
+        self._layer_name = layer_name
+        for field_type in fields:
+            self._fields[field_type] = ""
+            setattr(self, field_type, "")
+
+    def _get_property_values(self, name: str) -> str:
+        return self._fields[name]
+
+    def _get_property_types(self, name: str) -> Type[str]:
+        return str
+
+    def _get_layer_name(self) -> str:
+        return self._layer_name
--- packages/map_editor/layers.py
+++ packages/map_editor/layers.py
@@ -4,9 +4,8 @@ from typing import Any, Dict
 from utils.constants import TILES, TILE_SIZE, TILE_MAPS, WATCHTOWERS, \
     FRAMES, TRAFFIC_SIGNS, GROUND_TAGS, VEHICLES, CITIZENS
 from mapStorage import MapStorage
-from classes.layers import AbstractLayer
+from classes.layers import BasicLayerHandler
 from classes.basic.command import Command
-from classes.basic.chain import AbstractHandler
 from classes.MapDescription import MapDescription
 from classes.Commands.GetLayerCommand import GetLayerCommand
 from dt_maps.types.watchtowers import WatchtowerType
@@ -16,182 +15,144 @@ from dt_maps.types.citizens import CitizenType
 from dt_maps.types.vehicles import VehicleType, ColorType
 
 
-class TileLayerHandler(AbstractHandler, AbstractLayer):
-    def __init__(self, *kwargs) -> None:
-        super(TileLayerHandler, self).__init__(*kwargs)
+class TilesLayerHandler(BasicLayerHandler):
+    def __init__(self) -> None:
+        super(TilesLayerHandler, self).__init__(TILES)
+        self._default_conf = {'i': 0, 'j': 0, 'k': 0, 'type': 'floor'}
 
     def handle(self, command: Command) -> Any:
-        response = command.execute(self.dm, self.data, self.layer_name(),
+        response = command.execute(self.dm, self.data, self.layer_name,
                                    self.default_conf(),
                                    check_config=self.check_config)
         if response:
             return response
         return super().handle(command)
-
-    def layer_name(self) -> str:
-        return TILES
-
-    def default_conf(self) -> Dict[str, Any]:
-        return {'i': 0, 'j': 0, 'type': 'floor'}
 
     def check_config(self, config: Dict[str, Any]) -> bool:
         return super().check_config(config) and config.get("type") \
                in [t.value for t in TileType]
 
 
-class WatchtowersLayerHandler(AbstractHandler, AbstractLayer):
-    def __init__(self, *kwargs) -> None:
-        super(WatchtowersLayerHandler, self).__init__(*kwargs)
+class WatchtowersLayerHandler(BasicLayerHandler):
+    def __init__(self) -> None:
+        super(WatchtowersLayerHandler, self).__init__(WATCHTOWERS)
+        self._default_conf = {'configuration': 'WT18', 'id': ""}
 
     def handle(self, command: Command) -> Any:
-        response = command.execute(self.dm, self.data, self.layer_name(),
+        response = command.execute(self.dm, self.data, self.layer_name,
                                    self.default_conf(),
                                    check_config=self.check_config)
         if response:
             return response
         return super().handle(command)
-
-    def layer_name(self) -> str:
-        return WATCHTOWERS
-
-    def default_conf(self) -> Dict[str, str]:
-        return {'configuration': 'WT18', 'id': ""}
 
     def check_config(self, config: Dict[str, Any]) -> bool:
         return super().check_config(config) and config.get("configuration") \
                in [t.value for t in WatchtowerType]
 
 
-class FramesLayerHandler(AbstractHandler, AbstractLayer):
-    def __init__(self, *kwargs) -> None:
-        super(FramesLayerHandler, self).__init__(*kwargs)
+class FramesLayerHandler(BasicLayerHandler):
+    def __init__(self) -> None:
+        super(FramesLayerHandler, self).__init__(FRAMES)
+        self._default_conf = {'pose': {'x': 1.0, 'y': 1.0, 'z': 0.0, 'yaw': 0.0, 'roll': 0.0,
+                         'pitch': 0.0}, 'relative_to': ""}
 
     def handle(self, command: Command) -> Any:
-        response = command.execute(self.dm, self.data, self.layer_name(),
+        response = command.execute(self.dm, self.data, self.layer_name,
                                    self.default_conf(),
                                    check_config=self.check_config)
         if response:
             return response
         return super().handle(command)
 
-    def layer_name(self) -> str:
-        return FRAMES
 
-    def default_conf(self) -> Dict[str, Any]:
-        return {'pose': {'x': 1.0, 'y': 1.0, 'z': 0.0, 'yaw': 0.0, 'roll': 0.0,
-                         'pitch': 0.0}, 'relative_to': ""}
-
-
-class TileMapsLayerHandler(AbstractHandler, AbstractLayer):
-    def __init__(self, *kwargs) -> None:
-        super(TileMapsLayerHandler, self).__init__(*kwargs)
+class TileMapsLayerHandler(BasicLayerHandler):
+    def __init__(self) -> None:
+        super(TileMapsLayerHandler, self).__init__(TILE_MAPS)
+        self._default_conf = {TILE_SIZE: {'x': 0.585, 'y': 0.585}}
 
     def handle(self, command: Command) -> Any:
-        response = command.execute(self.dm, self.data, self.layer_name(),
+        response = command.execute(self.dm, self.data, self.layer_name,
                                    self.default_conf())
         if response:
             return response
         return super().handle(command)
 
-    def layer_name(self) -> str:
-        return TILE_MAPS
 
-    def default_conf(self) -> Dict[str, Any]:
-        return {TILE_SIZE: {'x': 0.585, 'y': 0.585}}
-
-
-class TrafficSignsHandler(AbstractHandler, AbstractLayer):
-    def __init__(self, *kwargs) -> None:
-        super(TrafficSignsHandler, self).__init__(*kwargs)
+class TrafficSignsLayerHandler(BasicLayerHandler):
+    def __init__(self) -> None:
+        super(TrafficSignsLayerHandler, self).__init__(TRAFFIC_SIGNS)
+        self._default_conf = {"type": "stop", "id": 0, "family": "36h11"}
 
     def handle(self, command: Command) -> Any:
-        response = command.execute(self.dm, self.data, self.layer_name(),
+        response = command.execute(self.dm, self.data, self.layer_name,
                                    self.default_conf(),
                                    check_config=self.check_config)
         if response:
             return response
         return super().handle(command)
-
-    def layer_name(self) -> str:
-        return TRAFFIC_SIGNS
-
-    def default_conf(self) -> Dict[str, Any]:
-        return {"type": "stop", "id": 0, "family": "36h11"}
 
     def check_config(self, config: Dict[str, Any]) -> bool:
         return super().check_config(config) and \
                config.get("type") in [t.value for t in TrafficSignType]
 
 
-class GroundTagsHandler(AbstractHandler, AbstractLayer):
-    def __init__(self, *kwargs) -> None:
-        super(GroundTagsHandler, self).__init__(*kwargs)
+class GroundTagsLayerHandler(BasicLayerHandler):
+    def __init__(self) -> None:
+        super(GroundTagsLayerHandler, self).__init__(GROUND_TAGS)
+        self._default_conf = {"size": 0.15, "id": 0, "family": "36h11"}
 
     def handle(self, command: Command) -> Any:
-        response = command.execute(self.dm, self.data, self.layer_name(),
+        response = command.execute(self.dm, self.data, self.layer_name,
                                    self.default_conf(),
                                    check_config=self.check_config)
         if response:
             return response
         return super().handle(command)
 
-    def layer_name(self) -> str:
-        return GROUND_TAGS
 
-    def default_conf(self) -> Dict[str, Any]:
-        return {"size": 0.15, "id": 0, "family": "36h11"}
-
-
-class CitizensHandler(AbstractHandler, AbstractLayer):
-    def __init__(self, *kwargs) -> None:
-        super(CitizensHandler, self).__init__(*kwargs)
+class CitizensLayerHandler(BasicLayerHandler):
+    def __init__(self) -> None:
+        super(CitizensLayerHandler, self).__init__(CITIZENS)
+        self._default_conf = {"color": "yellow"}
 
     def handle(self, command: Command) -> Any:
-        response = command.execute(self.dm, self.data, self.layer_name(),
+        response = command.execute(self.dm, self.data, self.layer_name,
                                    self.default_conf(),
                                    check_config=self.check_config)
         if response:
             return response
         return super().handle(command)
-
-    def layer_name(self) -> str:
-        return CITIZENS
-
-    def default_conf(self) -> Dict[str, Any]:
-        return {"color": "yellow"}
 
     def check_config(self, config: Dict[str, Any]) -> bool:
         return super().check_config(config) and \
                config.get("color") in [t.value for t in CitizenType]
 
 
-class VehiclesHandler(AbstractHandler, AbstractLayer):
-    def __init__(self, *kwargs) -> None:
-        super(VehiclesHandler, self).__init__(*kwargs)
+'''class VehiclesHandler(BasicLayer):
+    def __init__(self) -> None:
+        super(VehiclesHandler, self).__init__(VEHICLES)
 
     def handle(self, command: Command) -> Any:
-        response = command.execute(self.dm, self.data, self.layer_name(),
+        response = command.execute(self.dm, self.data, self.layer_name,
                                    self.default_conf(),
                                    check_config=self.check_config)
         if response:
             return response
         return super().handle(command)
 
-    def layer_name(self) -> str:
-        return VEHICLES
-
     def default_conf(self) -> Dict[str, Any]:
-        return {"color": "blue", "configuration": "DB18", "id": ""}
+        return {"color": "blue", "configuration": "DB18", "id": 0}
 
     def check_config(self, config: Dict[str, Any]) -> bool:
         return super().check_config(config) and \
                config.get("configuration") in [t.value for t in VehicleType] and \
-               config.get("color") in [t.value for t in ColorType]
+               config.get("color") in [t.value for t in ColorType]'''
 
 
 if __name__ == '__main__':
     MapStorage(MapDescription(Path("./maps/tm1"), "test"))
-    tile_layer = TileLayerHandler()
+    tile_layer = TilesLayerHandler()
     watchtower_layer = WatchtowersLayerHandler()
     tile_layer.set_next(watchtower_layer)
     layer = tile_layer
