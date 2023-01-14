7ebc708f81bf1a78bd95c4dfd4899f5c2e20b923
Valentina-Gol <vip.valya.golubeva@mail.ru>
2023-01-14 17:12:37 +0300

    show dirs only

--- packages/map_editor/utils/qtWindowAPI.py
+++ packages/map_editor/utils/qtWindowAPI.py
@@ -7,12 +7,14 @@ class QtWindowAPI:
     def __init__(self, cur_dir: str):
         self.dir = cur_dir
     def get_dir(self, parent: QtWidgets.QWidget, info: str = "") -> str:
-        return QFileDialog.getExistingDirectory(parent, f"Select a folder to {info} the map", self.dir,
-                                                          QFileDialog.ShowDirsOnly | QFileDialog.DontResolveSymlinks)
+        return QFileDialog.getExistingDirectory(parent, f"Select a folder to {info} the map",
+                                                self.dir,
+                                                options=QFileDialog.DontUseNativeDialog | QFileDialog.ShowDirsOnly)
 
     def create_file_name(self, parent: QtWidgets.QWidget, info: str = "filename") -> str:
         if "SNAP" in os.environ:
-            return QFileDialog.getSaveFileName(parent, f"Select {info} to save", self.dir, options=QtWidgets.QFileDialog.DontUseNativeDialog)[0]
+            return QFileDialog.getSaveFileName(parent, f"Select {info} to save",
+                                               self.dir, options=QtWidgets.QFileDialog.DontUseNativeDialog)[0]
         else:
             return QFileDialog.getSaveFileName(parent, f"Select {info} to save",
                                                self.dir)[0]
